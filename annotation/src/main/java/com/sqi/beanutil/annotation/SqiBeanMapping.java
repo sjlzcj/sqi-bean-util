6d8f497b435d527ed6f9b256fb439105f1dd1278
宋金龙 <songjinlong@songjinlongdeMacBook-Pro.local>
2023-01-07 22:29:11 +0800

    first commit

---
+++ annotation/src/main/java/com/sqi/beanutil/annotation/SqiBeanMapping.java
@@ -0,0 +1,13 @@
+package com.sqi.beanutil.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface SqiBeanMapping {
+    String source() default "";
+    String target() default "";
+}
---
+++ processor/src/main/java/com/sqi/beanutil/processor/SqiAnnotationProcessor.java
@@ -0,0 +1,44 @@
+package com.sqi.beanutil.processor;
+
+import com.google.auto.service.AutoService;
+import com.sqi.beanutil.annotation.SqiBeanMapping;
+import com.sqi.beanutil.annotation.SqiBeanMappings;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class SqiAnnotationProcessor extends AbstractProcessor {
+
+    private ProcessingEnvironment processingEnv;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new LinkedHashSet<String>();
+        annotations.add(SqiBeanMappings.class.getCanonicalName());
+        annotations.add(SqiBeanMapping.class.getCanonicalName());
+        return annotations;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("SQI.PROCESSOR--->" + getSupportedAnnotationTypes());
+        return true;
+    }
+}
