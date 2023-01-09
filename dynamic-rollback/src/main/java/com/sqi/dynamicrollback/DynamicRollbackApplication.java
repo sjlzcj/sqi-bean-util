4014404af78111d7c8218854286c0c16f013e6c7
zhangfuxue <zhangfuxue@58.com>
2023-01-09 11:11:13 +0800

    1

---
+++ dynamic-rollback/src/main/java/com/sqi/dynamicrollback/DynamicRollbackApplication.java
@@ -0,0 +1,13 @@
+package com.sqi.dynamicrollback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRollbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicRollbackApplication.class, args);
+    }
+
+}
---
+++ dynamic-rollback/src/test/java/com/sqi/dynamicrollback/DynamicRollbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqi.dynamicrollback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRollbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
